f107399d02f271e9ce66103d2d0c7decc2f781ac
Bomne <90371987+Bomne@users.noreply.github.com>
2023-06-20 23:17:27 +0700

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-
-  // Kiểm tra thông tin đăng nhập và xử lý logic tại đây
-}
-?>
